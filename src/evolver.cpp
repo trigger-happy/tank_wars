df7579706433d96de4d30a83cc27f6f732b2a4d1
James Choa <triggerhappyelite@gmail.com>
2010-07-17 06:11:53 +0800

    Added some base code

---
+++ src/evolver.cpp
@@ -0,0 +1,33 @@
+/*
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License version 2 as published by the Free Software Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public License
+	along with this library; see the file COPYING.LIB.  If not, write to
+	the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+	Boston, MA 02110-1301, USA.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[]){
+	po::options_description desc;
+	desc.add_options()
+		("help", "display this help message");
+		
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+	return 0;
+}
