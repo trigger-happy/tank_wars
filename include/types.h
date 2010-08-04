652b72a4cfbcbbc845e978a86ed132e449c680b6
James Choa <triggerhappyelite@gmail.com>
2010-08-04 13:10:15 +0800

    Added the data types

---
+++ include/types.h
@@ -0,0 +1,32 @@
+/*
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License version 2 as published by the Free Software Foundation.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public License
+	along with this library; see the file COPYING.LIB.  If not, write to
+	the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+	Boston, MA 02110-1301, USA.
+*/
+#ifndef TYPES_H
+#define TYPES_H
+#include <boost/cstdint.hpp>
+
+typedef boost::uint8_t	u8;
+typedef boost::uint16_t	u16;
+typedef boost::uint32_t	u32;
+typedef boost::uint64_t	u64;
+typedef boost::int8_t	s8;
+typedef boost::int16_t	s16;
+typedef boost::int32_t	s32;
+typedef boost::int64_t	s64;
+
+typedef float			f32;
+typedef double			f64;
+
+#endif //TYPES_H
